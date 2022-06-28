a858a154e5c65f21235a7d83578ccd49ad1b1a87
Enderman-Somnath <91421328+Enderman-Somnath@users.noreply.github.com>
2022-06-28 17:05:05 +0530

    Add files via upload

---
+++ My Website/Functions/functions.js
@@ -0,0 +1,16 @@
+// homeWeb() classicChatWeb() youtubeWeb() aboutWeb() donationWeb()
+function homeWeb(){
+    window.location="/index.html"
+}
+function classicChatWeb(){
+    window.location="/ClassicChat/ClassicChatLogin.html"
+}
+function youtubeWeb(){
+    window.location="/Youtube/youtube.html"
+}
+function aboutWeb(){
+
+}
+function donationWeb(){
+    window.location="/Donation/Donation.html"
+}
