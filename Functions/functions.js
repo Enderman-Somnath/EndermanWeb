2e355add94278cc38a4ba0e38908dd2f9d1d8af4
Enderman-Somnath <91421328+Enderman-Somnath@users.noreply.github.com>
2022-06-28 17:14:38 +0530

    Update functions.js

--- Functions/functions.js
+++ Functions/functions.js
@@ -1,16 +1,16 @@
 // homeWeb() classicChatWeb() youtubeWeb() aboutWeb() donationWeb()
 function homeWeb(){
-    window.location="EndermanWeb/index.html"
+    window.location="/EndermanWeb/index.html"
 }
 function classicChatWeb(){
-    window.location="EndermanWeb/ClassicChat/ClassicChatLogin.html"
+    window.location="/EndermanWeb/ClassicChat/ClassicChatLogin.html"
 }
 function youtubeWeb(){
-    window.location="EndermanWeb/Youtube/youtube.html"
+    window.location="/EndermanWeb/Youtube/youtube.html"
 }
 function aboutWeb(){
 
 }
 function donationWeb(){
-    window.location="EndermanWeb/Donation/Donation.html"
+    window.location="/EndermanWeb/Donation/Donation.html"
 }
